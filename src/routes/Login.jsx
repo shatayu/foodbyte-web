8de8fa0ee5353b320541ae380e9678e28c63e8eb
shatayu <shatayu@outlook.com>
2018-09-15 15:02:03 -0400

    removed firebase, unused vars in login warnings

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import firebase from 'firebase';
+let firebase = require("firebase/app");
+require("firebase/auth");
 
 class Login extends React.Component {
   login() {
@@ -24,20 +25,20 @@ class Login extends React.Component {
 
     firebase.auth().signInWithPopup(provider).then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
+      //var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
       console.log(user);
       // ...
     }).catch(function(error) {
-      // Handle Errors here.
-      console.log(error);
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+      // // Handle Errors here.
+      // console.log(error);
+      // var errorCode = error.code;
+      // var errorMessage = error.message;
+      // // The email of the user's account used.
+      // var email = error.email;
+      // // The firebase.auth.AuthCredential type that was used.
+      // var credential = error.credential;
       // ...
     });
 
@@ -65,7 +66,7 @@ class Login extends React.Component {
             }
           }
         >
-          Iguana
+          TopPepe
         </button>
       </div>
     );
