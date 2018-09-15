1a9da7a32922226e237b2293fcfedcf6bc1712bc
Blake Johnson <blake.k.johnson.4@gmail.com>
2018-09-15 19:31:05 -0400

    Switched to the yellow home boy

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import STYLE_CONSTS from '../style.js';
+import { Home } from 'react-feather';
 
 const HomeButton = () => {
   return (
     <Link to='/' style={{
       position: 'absolute',
       top: '15px',
-      right: '15px'
+      right: '15px',
+      textDecoration: 'none'
     }}>
-      <img src={require('../home.svg')} style={{
-        height: '62.5px'
-      }}/>
+      <Home color={STYLE_CONSTS.COLORS.YELLOW} size={50} />
     </Link>
   );
 };
