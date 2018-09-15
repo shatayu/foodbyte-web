661ed0726d5758ea21362ef2942cd5c62356ab4f
Blake Johnson <blake.k.johnson.4@gmail.com>
2018-09-15 19:22:28 -0400

    Added React feather

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import STYLE_CONSTS from '../style.js';
 
 const HomeButton = () => {
-  return <Link to='/'>Home</Link>;
+  return (
+    <Link to='/' style={{
+      position: 'absolute',
+      top: '15px',
+      right: '15px'
+    }}>
+      <img src={require('../home.svg')} style={{
+        height: '62.5px'
+      }}/>
+    </Link>
+  );
 };
 export default HomeButton;
