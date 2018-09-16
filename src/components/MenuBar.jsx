acba5e41fb3bb67852093cd2a996749f9755ad9c
rishyraj <rishyraj10@gmail.com>
2018-09-16 00:33:50 -0400

    Resized Menubar icons from size 50 to size 45

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -53,27 +53,27 @@ class MenuBar extends React.Component {
         right: '25px'
       }}>
         <Link to='/favorites'>
-          <Star size={50}
-            onMouseEnter={this.starEnter.bind(this)} 
-            onMouseLeave={this.starLeave.bind(this)} 
+          <Star size={45}
+            onMouseEnter={this.starEnter.bind(this)}
+            onMouseLeave={this.starLeave.bind(this)}
             color={starColor} style={margin} />
         </Link>
         <Link to='/recommended'>
-          <Sun size={50} 
-            onMouseEnter={this.bookEnter.bind(this)} 
-            onMouseLeave={this.bookLeave.bind(this)} 
+          <Sun size={45}
+            onMouseEnter={this.bookEnter.bind(this)}
+            onMouseLeave={this.bookLeave.bind(this)}
             color={bookColor} style={margin} />
         </Link>
         <Link to='/profile'>
-          <User size={50}
+          <User size={45}
               onMouseEnter={this.userEnter.bind(this)}
               onMouseLeave={this.userLeave.bind(this)}
               color={userColor} style={margin} />
         </Link>
         <Link to='/'>
-          <Home size={50} 
-            onMouseEnter={this.homeEnter.bind(this)} 
-            onMouseLeave={this.homeLeave.bind(this)} 
+          <Home size={45}
+            onMouseEnter={this.homeEnter.bind(this)}
+            onMouseLeave={this.homeLeave.bind(this)}
             color={homeColor} style={margin} />
         </Link>
       </div>
