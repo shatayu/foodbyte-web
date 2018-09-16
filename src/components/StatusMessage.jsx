cf441c85386d7e7b9c5f5365e46e0297cb26f338
shatayu <shatayu@outlook.com>
2018-09-15 20:10:17 -0400

    styled loading message for search

--- src/components/StatusMessage.jsx
+++ src/components/StatusMessage.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import style from './statusMessage';
+let style = require('../style');
 
 const StatusMessage = ({ status }) => {
   let message = status ? 'We are fetching your recipes!' : '';
-  return <div style={style}>{message}</div>;
+
+  let loading = {
+    fontSize: '32px',
+    fontFamily: 'helvetica',
+    color: style.default.COLORS.YELLOW
+  }
+
+  let container = {
+    textAlign: 'center',
+    color: style.default.COLORS.WHITE,
+    posiion: 'relative',
+    width: '70%',
+    height: '70%'
+  };
+
+  let inner = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: '50%',
+    transform: 'translate(-50%, -50%)',
+    
+    fontSize: '30px',
+    fontFamily: 'helvetica',
+    color: style.default.COLORS.YELLOW
+  }
+
+  console.log("status is " + status);
+  if (!status) {
+    return (<div style={container}>
+      <div style={inner}>
+        {''}
+      </div>
+    </div>);
+  } else {
+    return (<div style={container}>
+      <div style={inner}>
+        <span style={loading}>LOADING...</span>
+      </div>
+    </div>);
+  }
+
 };
 export default StatusMessage;
